2a61b267c53f822adf3259deb7daec53a17c1897
Leonard <leonard.horne@gmail.com>
2017-08-26 14:21:40 -0400

    Show total hand value to user.

--- starter_files/blackjack.rb
+++ starter_files/blackjack.rb
@@ -1,25 +1,30 @@
-require_relative 'deck'
-require_relative 'game'
-require_relative 'user'
-require_relative 'dealer'
+require_relative 'lib/deck'
+# require_relative 'lib/game'
+require_relative 'lib/user'
+require_relative 'lib/dealer'
 
+# Game.new
 puts "Hello and welcome to the game of blackjack! Let's begin."
 
-user = User.new
-dealer = Dealer.new
+user = User.new([], 100)
+puts "You have #{user.money} and bet $10"
+
+dealer = Dealer.new([])
+
 deck = Deck.new
 deck.shuffle
 
 
 user.hand << user.draw * 2
+puts "You have a #{user.hand[0].to_i} and a #{user.hand[1].to_i}. Your total is #{user.hand.sum}"
 dealer.hand << dealer.draw * 2
-
-
-puts "Do you want to hit or stand?"
-user_move = gets.chomp.to_s
-
-if user_move == "H"
-  user.hit
-else
-  user.stand
-end
+# #
+# #
+# # puts "Do you want to hit or stand?"
+# # user_move = gets.chomp.to_s
+# #
+# # if user_move == "H"
+# #   user.hit
+# # else
+# #   user.stand
+# # end
