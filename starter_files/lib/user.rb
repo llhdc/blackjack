807c47396d04e2ef33330a2d3689ff6566adefdf
Leonard <leonard.horne@gmail.com>
2017-08-27 10:18:46 -0400

    Define hand_value method.

--- starter_files/lib/user.rb
+++ starter_files/lib/user.rb
@@ -4,16 +4,29 @@ class User
   def initialize(hand, money)
     @hand = hand
     @money = money
+    @hand_value = [].sum
   end
-  
+
+  def hand
+    @hand
+  end
+
+  def hand_value
+    if self.value == 1
+      @user.hand_value = 11
+    elsif self.value == 2..9
+      @user.hand_value + 1
+    elsif self.value == 10 || self.value == 11 || self.value == 12
+      @user.hand_value
+    end
+  end
+
   def money
     @money
   end
 
-  def hit
-    # push drawn card to hand array
-    @hand = []
-    self.hand << deck.draw
+  def hand_value
+    @hand_value
   end
 
   def stand
