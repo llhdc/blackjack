8e83f4b8d09f8dfd4578d9419293b8501e4ba029
Leonard <leonard.horne@gmail.com>
2017-09-26 13:42:36 -0400

    Extract shared logic between user and deler into one player class.

--- starter_files/lib/Player.rb
+++ starter_files/lib/Player.rb
@@ -1,13 +1,12 @@
 require_relative 'deck'
 
-class Dealer
-  def initialize
+class Player
+  attr_reader :hand, :money, :name
+  def initialize(name)
     @hand = []
+    @money = 100
+    @name = name
     # @hand_value = 0
-  end
-
-  def hand
-    @hand
   end
 
   # def hand_value
