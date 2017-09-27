8f7068440a3c08cf279b52e61c259da1309992e0
Leonard <leonard.horne@gmail.com>
2017-09-27 11:33:27 -0400

    Remove game logic from blackjack file.

--- starter_files/blackjack.rb
+++ starter_files/blackjack.rb
@@ -1,12 +1,3 @@
 require_relative 'lib/game'
 
-puts "Hello and welcome to the game of blackjack! Let's begin."
-game = Game.new
-
-game.start
-
-
-
-# until loop for rounds
-until game.user.money == 0
-end
+game = Game.new.start
